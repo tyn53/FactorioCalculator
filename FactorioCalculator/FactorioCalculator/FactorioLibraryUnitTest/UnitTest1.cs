c4bb0c8ecf5b9e2b7480df5e100dea1ff47c8747
tyn53 <jbush53@gmail.com>
2018-01-23 22:30:51 -0600

    Added Unit Test project for Factorio Library
    Added Unit Test project for Factorio Library

---
+++ FactorioCalculator/FactorioCalculator/FactorioLibraryUnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactorioLibraryUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
